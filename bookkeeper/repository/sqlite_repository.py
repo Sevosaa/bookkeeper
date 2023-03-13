33d03ea045e7cf90dc9776dc9d3da992ebad099b
Sevosaa <lexxa.sevostyanova@mail.ru>
2023-03-13 13:45:37 +0300

    commented func add was removed from sqliterepository

--- bookkeeper/repository/sqlite_repository.py
+++ bookkeeper/repository/sqlite_repository.py
@@ -23,18 +23,6 @@ class SQLiteRepository(AbstractRepository[T]):
                 cur.execute(q)
         con.close()
 
-    # def add(self, obj: T) -> int:
-    #     names = ', '.join(self.fields.keys())
-    #     p = ', '.join("?" * len(self.fields))
-    #     values = [getattr(obj, x) for x in self.fields]
-    #     with sqlite3.connect(self.db_file) as con:
-    #         cur = con.cursor()
-    #         cur.execute('PRAGMA foreign_keys = ON')
-    #         q = f'INSERT INTO {self.table_name} ({names}) VALUES ({p})'
-    #         cur.execute(q, values)
-    #         obj.pk = cur.lastrowid
-    #     con.close()
-    #     return obj.pk
     def add(self, obj: T) -> int:
         names = ', '.join(self.fields.keys())
         p = ', '.join("?" * len(self.fields))
